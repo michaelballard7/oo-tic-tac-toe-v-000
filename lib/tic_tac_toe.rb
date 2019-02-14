75e35b31d5d943cc5d6deb418bb8cc6b10558fb7
Michael Ballard <ballard.michael9@gmail.com>
2019-02-14 01:37:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,10 +29,9 @@ class TicTacToe
   end
 
   def position_taken? index
-  if @board[index] == 'X' || @board[index] == 'O'
-    true
-  elsif @board[index] == "" || @board[index] == " "
-    false
+    if @board[index] == 'X' || @board[index] == 'O'
+      true
+    elsif @board[index] == "" || @board[index] == " "
+      false
   end
-
 end
